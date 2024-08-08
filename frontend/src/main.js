47ef6a2cf87f077fa68cf92174f3217a5d567e16
mindong <74493250+mindongdong@users.noreply.github.com>
2024-08-09 04:48:23 +0900

    update build

--- frontend/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-createApp(App).use(store).use(router).mount('#app')
